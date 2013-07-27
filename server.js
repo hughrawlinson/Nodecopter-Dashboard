30501dd6a988b66aa36e67eae4cd9c508cf4c2aa
Hugh Rawlinson <hugh@codeoclock.net>
2013-07-27 11:21:41 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var app = require('http').createServer(handler)
+	, io = require('socket.io').listen(app)
+	, fs = require('fs')
+
+app.listen(80);
+
+function handler (req, res) {
+	var requestedFile = req.url.substring(1);
+	if (requestedFile == ""||requestedFile[requestedFile.length-1]=="/"){
+		requestedFile += "index.html";
+	}
+	console.log(requestedFile);
+	fs.readFile(requestedFile,function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+		res.writeHead(200);
+		res.write(data);
+		res.end();
+	});
+}
+
+io.sockets.on('connection', function (socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+	});
+});
